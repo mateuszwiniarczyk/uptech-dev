9e3e1206b772532979e7ece9abefa732784dcbe9
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2022-12-10 16:37:32 +0100

    feat: create custom tailwind colors and styles

---
+++ src/features/auth/routes/Login.tsx
@@ -0,0 +1,3 @@
+const Login = () => <h1 className='text-primary-200'>Login</h1>;
+
+export { Login };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,44 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      black: 'var(--color-black)',
+      white: 'var(--color-white)',
+      primary: {
+        100: 'var(--color-primary-100)',
+        200: 'var(--color-primary-200)',
+        300: 'var(--color-primary-300)',
+      },
+      orange: {
+        100: 'var(--color-orange-100)',
+        200: 'var(--color-orange-200)',
+      },
+      green: {
+        100: 'var(--color-green-100)',
+        200: 'var(--color-green-200)',
+      },
+      pink: {
+        100: 'var(--color-pink-100)',
+        200: 'var(--color-pink-200)',
+      },
+      slate: {
+        100: 'var(--color-slate-100)',
+        200: 'var(--color-slate-200)',
+        300: 'var(--color-slate-300)',
+      },
+      gray: {
+        100: 'var(--color-gray-100)',
+        200: 'var(--color-gray-200)',
+        300: 'var(--color-gray-300)',
+        400: 'var(--color-gray-400)',
+        500: 'var(--color-gray-500)',
+        600: 'var(--color-gray-600)',
+        700: 'var(--color-gray-700)',
+        800: 'var(--color-gray-800)',
+      },
+    },
   },
   plugins: [],
 };
